dbf9934ba9567f4e3f4b5a3590c3896054fb6e39
OxygenMemo <g58160698@gmail.com>
2017-11-12 01:06:23 +0700

    เอาจริงแล้วนะ

--- KINDEE/application/views/script/configmap.js
+++ KINDEE/application/views/script/configmap.js
@@ -1,5 +1,15 @@
 
-      let map ;
-      let initMap = () => {
-          map = new google.maps.Map(document.getElementById('map'))
-      }
+    let map ;
+    let positionInit={
+        lat: 13.312277, 
+        lng: 100.532970
+    }
+    //13.312277, 100.532970 center of thailand 
+    let mapInitOption={
+        center : positionInit, 
+        zoom : 0
+    }
+    
+    let initMap = () => { // initiation map
+        map = new google.maps.Map(document.getElementById('map'),positionInit)
+    } // end function initMap
