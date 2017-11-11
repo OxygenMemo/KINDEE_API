08dd0afb46f105e8dad97df1c075d149c5bff85b
OxygenMemo <g58160698@gmail.com>
2017-11-12 02:16:45 +0700

    test

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -60,8 +60,9 @@
 
     function autoLoadCurrentLocation(){ // auto load current location
       setInterval(getCurrentLocation(),3000);
+      console.log("lat : " + currentPosition.lat + " lng : " + currentPosition.lng)
     }// end function autoload Current
-    getCurrentLocation();
+    
 
     
     </script>
