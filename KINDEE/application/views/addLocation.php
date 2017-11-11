7a7b3331b890cd5e4194d25858ef8f58cfc667ac
OxygenMemo <g58160698@gmail.com>
2017-11-12 00:36:19 +0700

    n

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -56,7 +56,7 @@
             infoWindow.setContent('Location found.');
             
             infoWindow.open(map);
-            map.setZoom(9);
+            map.setZoom(1);
             map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
