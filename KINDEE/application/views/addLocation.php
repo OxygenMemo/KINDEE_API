d4016f6be4ebefab01c7858169762d2feb25125f
OxygenMemo <g58160698@gmail.com>
2017-11-12 00:32:33 +0700

    g

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -43,6 +43,10 @@
         map.panTo(latLng);
       }
       infoWindow = new google.maps.InfoWindow;
+      var shape = {
+          coords: [1, 1, 1, 20, 18, 20, 18, 1],
+          type: 'poly'
+        };
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -54,7 +58,8 @@
             var marker = new google.maps.Marker({
               position: pos,
               map: map,
-              icon : "./img_pin/mepin.png"
+              icon : "./img_pin/mepin.png",
+              shape : shape
             });
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
