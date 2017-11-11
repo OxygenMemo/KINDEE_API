e1f71803ae3162d7d3d487133e3be8a7719c0698
OxygenMemo <g58160698@gmail.com>
2017-11-11 20:38:09 +0700

    fix

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -15,7 +15,7 @@
       let map , infoWindow,pos
       function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
           center: uluru
         });
