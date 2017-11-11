9ed91bd7af665b12b8aa085299bed252dc5d4322
OxygenMemo <g58160698@gmail.com>
2017-11-11 20:42:46 +0700

    kuy

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -1,51 +1,59 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title>Geolocation</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
     <style>
-       #map {
-        height: 400px;
-        width: 100%;
-       }
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
     </style>
   </head>
   <body>
-    <h3>My Google Maps Demo</h3>
     <div id="map"></div>
     <script>
-      let map , infoWindow,pos
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
       function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: uluru
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
         });
-        
         infoWindow = new google.maps.InfoWindow;
 
-        if(navigator.geolocation){
-          navigator.geolocation.getCurrentPosition((position)=>{
-            pos={
-              lat : position.coords.latitude,
-              lng : position.coords.longitude
-            }
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-            infoWindow.setPosition(pos)
-            infoWindow.setContent('Ho yes that work !')
-            infoWindow.open(map)
-            map.setCenter(pos)
-          },()=>{
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
-
           });
-
-        }//if
-        else {
+        } else {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
-
         }
-        
-      }//init map
+      }
+
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
@@ -53,11 +61,9 @@
                               'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
       }
-
     </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiCH89cXrEofWGXV4OjTk6nF3LXIZ2HDs&callback=initMap">
     </script>
   </body>
 </html>
-      
