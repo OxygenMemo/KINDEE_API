434ee98332f3b0e3b4901f58cbe6ea8701f683de
OxygenMemo <g58160698@gmail.com>
2017-11-12 01:41:04 +0700

    fig init zoom 8

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -31,7 +31,7 @@
     //13.312277, 100.532970 center of thailand 
     var mapInitOption={
         center : positionInit, 
-        zoom : 5
+        zoom : 8
     };
     
     function initMap(){ // initiation map
