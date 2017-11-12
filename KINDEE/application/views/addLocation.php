fb859e97bc70175b467490171aebb5abe269fd65
OxygenMemo <g58160698@gmail.com>
2017-11-12 17:32:25 +0700

    s

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -8,7 +8,7 @@
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
-        width:70%;
+        height: 100%
       }
       /* Optional: Makes the sample page fill the window. */
       html, body {
@@ -18,12 +18,22 @@
       }
     </style>
     <script src='//maps.googleapis.com/maps/api/js'></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div id="map"></div>
-    <form>
-        <input type="text" >
-    </form>
+    <div class="container">  
+      <div class="col-sm-12">
+        <div class="col-sm-10">
+          <div id="map"></div>
+        </div>
+        <form>
+          <input type="text" >
+        </form>
+      </div>  
+    </div>
 
     <script>
     
