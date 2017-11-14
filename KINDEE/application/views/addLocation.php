fb3ad637169db427076095e45c219e745b91ff95
OxygenMemo <g58160698@gmail.com>
2017-11-14 22:59:27 +0700

    addLocation Version 1

--- KINDEE/application/views/addLocation.php
+++ KINDEE/application/views/addLocation.php
@@ -1,101 +1,116 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Geolocation</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+       #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
       }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      .sizemap{
-        height: 500px; 
-      }
+
     </style>
-    <script src='//maps.googleapis.com/maps/api/js'></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div class="container">  
-      <div class="col-sm-12">
-        
-        <div class="col-sm-10 sizemap">
-          <div id="map"></div>
-        </div>
-        <form>
-          <input type="text" >
-        </form>
-      </div>  
+    <h3>My Google Maps Demo</h3>
+    <div id="floating-panel">
+      <input type="button" onclick="myLocation()" value="myLocation">
     </div>
+    <div id="map"></div>
+    
+    <form>
+      <label for="lat" /> Longitude </label>
+      <input id="lat" name="lat" type="text" />
+      <br>
+      <label for="lng" /> Longitude </label>
+      <input id="lng" name="lng" type="text" /> 
+      <br>
+      <label for="Res_name" /> Name </label>
+      <input id="Res_name" name="Res_name" type="text" >
+      <br>
+      Detail <br>
+      <textarea id="Res_detail" name="Res_detail" cols="30" rows="5">
 
-    <script>
-    
-    var map;
-    var markCurrent = null;
-    
-    var currentPosition={lat:0,lng:0}; // user location now update with 
-    var zoom = {
-      countrysize : 7,
-      homesize : 15
-    }
-    
-    var positionInit={
-      lat : 13,
-      lng : 100
-    };
-    //13.312277, 100.532970 center of thailand 
-    var mapInitOption={
-        center : positionInit, 
-        zoom : zoom.countrysize
-    };
-    lat =1;
-    function initMap(){ // initiation map
-      console.log('Google Maps API version: ' + google.maps.version);
+      </textarea>
       
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-          lat = 1
-                    
+
+
+      <input type="submit" />
+    </form>
+    <script>
+      var markers = [];
+      var map;
+      function initMap() {//13.28642,100.9252583
+        var uluru = {lat: 13.28642, lng: 100.9252583};
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        
+        map.addListener("click",function(event){
+          showLatLng(event.latLng);
+          marker(map,event.latLng);
         });
       }
-      map = new google.maps.Map(document.getElementById('map'),mapInitOption);
+      function setAllMarker(map){
+        for(var i = 0 ; i < markers.length ; i++){
+            markers[i].setMap(map);
+        }
+      }
+      function marker(map,location){
+        setAllMarker(null);
+        markers = [];
+        markers.push(
+          new google.maps.Marker({
+            map : map,
+            position : location
+          })
+        );
+      }
+      function myLocation(){
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            
+            document.getElementById("lat").value = pos.lat;
+            document.getElementById("lng").value = pos.lng;
 
-      //console.log(currentPosition.lat);
-      google.maps.event.addListener(map, 'click', function(event) {
-        marker = new google.maps.Marker({position: event.latLng, map: map});
-      });
-      
-    } // end function initMap
-
-    function addmarker(location,map){
-          var mark = new google.maps.Marker({
-            position : location,
-            map : map
+            marker(map,pos);
+            map.setZoom(18);
+            map.setCenter(pos);
+          }, function() {
+            alert("error");
           });
+        } else {
+          // Browser doesn't support Geolocation
+          alert("error");
+        }
         
-    }
-    
-    
-    
-    //alert(currentPosition.lat);
+      }
 
-    
+      function showLatLng(location){
+        document.getElementById("lat").value = location.lat();
+        document.getElementById("lng").value = location.lng();
+      }
+
     </script>
-
-    <script 
-    src="https://maps.googleapis.com/maps/api/js?key= AIzaSyCo9tZvO6CghsfvS1H8mRF3TZkpEki3-DQ &callback=initMap" async defer>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXvLP913yXWox7MnNDgm7sVwtDpqZpCTM&callback=initMap">
     </script>
-
   </body>
 </html>
