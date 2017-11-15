176fb8fa9f181358d63858fee437a716ef286b5d
OxygenMemo <g58160698@gmail.com>
2017-11-15 09:42:30 +0700

    fig name addRestaurants

--- KINDEE/application/models/Restaurants.php
+++ KINDEE/application/models/Restaurants.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Restaurants extends CI_Model{
-    public function addLocation(){
+    public function addRestaurants(){
         $sql = "INSERT INTO `Restaurants`( `Res_name`, `Res_detail`, `Type_id`, `Res_lat`, `Res_lng`) VALUES (?,?,?,?,?)";
         $bind_data = array(
             $this->Res_name,
