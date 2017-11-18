5265e55b6a0cf5b7aec760c239ac96296ef3e75e
OxygenMemo <g58160698@gmail.com>
2017-11-18 21:07:12 +0700

    de bug check unique

--- KINDEE/application/models/User_DB.php
+++ KINDEE/application/models/User_DB.php
@@ -3,18 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_DB extends CI_Model{
     
     public function Register(){
+        if(!checkUnique($this->username))
+            return "F";
         $sql = "INSERT INTO `Users`(`User_username`, `User_password`, `User_fullname`) VALUES (?,?,?)";
         $data_bild = array(
             $this->username,
             sha1($this->password),
             $this->fullname);
-
+        
+        
         if($this->db->query($sql,$data_bild)){
             return "T";   
         }
         else{
             return "F";
         }
+    }
+    private function checkUnique($name){
+        $sqlcheckunique = "SELECT `User_username` FROM `Users` WHERE `User_name` LIKE ?";
+        $data_bild = array($name); 
+        $result = $this->db->query($sqlcheckunique,$data_bild);
+        return $result->num_rows() == 0 ? true : false;
     }
 
 }
