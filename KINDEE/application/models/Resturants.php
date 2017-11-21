c64cb91bdcb126e512eab9a798425e63c44de437
OxygenMemo <g58160698@gmail.com>
2017-11-21 20:10:04 +0700

    g

--- KINDEE/application/models/Resturants.php
+++ KINDEE/application/models/Resturants.php
@@ -19,7 +19,7 @@ class Resturants extends CI_Model{
 
     }
     public function getIDResturant(){
-        $sql = "SELECT id FROM imguploadtest ORDER BY id ASC";
+        $sql = "SELECT Res_id FROM Restaurants ORDER BY Res_id ASC";
         return $this->db->query($sql);
     }
     public function insertResturant(){
