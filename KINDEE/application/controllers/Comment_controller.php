d5fcd842d316c8f8267555c76b12b6064da51d38
OxygenMemo <g58160698@gmail.com>
2017-11-25 02:18:11 +0700

    fig json

--- KINDEE/application/controllers/Comment_controller.php
+++ KINDEE/application/controllers/Comment_controller.php
@@ -5,13 +5,12 @@ class Comment_controller extends CI_Controller{
 
     public function getAllCommentWithResId($Res_id){
         $this->load->model("Comment_DB");
-        $result = new obj();
+    
         $Comment_DB = $this->Comment_DB;
         $Comment_DB->Res_id = $Res_id;
-        $result->result = $Comment_DB->getAllCommentWithResId()->result();
+        $result = $Comment_DB->getAllCommentWithResId()->result();
 
         echo json_encode($result);
     }
 
 }
-class obj{}
