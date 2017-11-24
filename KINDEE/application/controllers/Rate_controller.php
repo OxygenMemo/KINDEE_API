685373cfb065c846b65a2c8a20d077726848686e
OxygenMemo <g58160698@gmail.com>
2017-11-25 00:18:16 +0700

    debug

--- KINDEE/application/controllers/Rate_controller.php
+++ KINDEE/application/controllers/Rate_controller.php
@@ -1,4 +1,18 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Rate_controller extends CI_Controller {
+    
+    public function getResRate($Res_id){
+        $this->load->model("Rate_DB");
+        $Rate_DB = $this->Rate_DB;
+        $Rate_DB->Res_id = $Res_id;
+
+        $result = $Rate_DB->getResRate();
+        echo json_encode($result);
+    }
+    public function getRateUser(){
+
+    }
     
 }
