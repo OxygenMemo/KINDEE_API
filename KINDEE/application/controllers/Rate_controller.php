83e1c929c69e0880bf92390de182240cc150a577
OxygenMemo <g58160698@gmail.com>
2017-11-25 03:41:09 +0700

    fig

--- KINDEE/application/controllers/Rate_controller.php
+++ KINDEE/application/controllers/Rate_controller.php
@@ -4,12 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Rate_controller extends CI_Controller {
     public function getResRate($Res_id){
         $this->load->model("Rate_DB");
-        $result = new obj();
+        
         $Rate_DB = $this->Rate_DB;
         $Rate_DB->Res_id = $Res_id;
-
-        $result->result = $Rate_DB->getResRate()->result();
-        echo json_encode($result);
+        $obj = new obj();
+        $result = $Rate_DB->getResRate();
+        $sum=0;
+        $num=$result->num_rows();
+        foreach($result->result() as $row){
+            $sum+=$row->Rate_number;
+        }
+        $sum/=$num;
+        $obj->Rate_number = $sum;
+        echo json_encode($sum);
     }
     public function getRateUser($Res_id,$User_id){
         $this->load->model("Rate_DB");
