a426a02d568ead573f5e6ed7759b809a512eadb8
OxygenMemo <g58160698@gmail.com>
2017-11-25 02:48:34 +0700

    update

--- KINDEE/application/models/Comment_DB.php
+++ KINDEE/application/models/Comment_DB.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Comment_DB extends CI_Model{
     public function getAllCommentWithResId(){
-        $sql = "SELECT c.Com_text,u.User_fullname FROM `Comments` c LEFT JOIN `Users` u ON c.User_id = u.User_id WHERE `Res_id` LIKE ? ";
+        $sql = "SELECT c.Com_text,u.User_fullname FROM `Comments` c LEFT JOIN `Users` u ON c.User_id = u.User_id WHERE `Res_id` LIKE ? group by c.Com_id desc limit 10";
         $data_bild = array($this->Res_id);
 
         return $this->db->query($sql,$data_bild);
