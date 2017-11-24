1676f34a830c6517b91178647aa6a7132870a1ab
OxygenMemo <g58160698@gmail.com>
2017-11-25 00:49:29 +0700

    update getRateUser

--- KINDEE/application/controllers/Rate_controller.php
+++ KINDEE/application/controllers/Rate_controller.php
@@ -11,8 +11,15 @@ class Rate_controller extends CI_Controller {
         $result->result = $Rate_DB->getResRate()->result();
         echo json_encode($result);
     }
-    public function getRateUser(){
-        echo "gg";
+    public function getRateUser($Res_id,$User_id){
+        $this->load->model("Rate_DB");
+        $result = new obj();
+        $Rate_DB = $this->Rate_DB;
+        $Rate_DB->Res_id = $Res_id;
+        $Rate_DB->User_id = $User_id;
+
+        $result->result = $Rate_DB->getRateUser()->result();
+        echo json_encode($result);
     }
     
 }
