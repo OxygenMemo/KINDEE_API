ecd50d73d4734199ab7a64393bfa07845d5d47e5
OxygenMemo <g58160698@gmail.com>
2017-11-25 00:32:32 +0700

    debug

--- KINDEE/application/models/Rete_DB.php
+++ KINDEE/application/models/Rete_DB.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+//if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Rate_DB extends CI_Model{
     
