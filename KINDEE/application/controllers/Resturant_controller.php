d3a1134546f77b6918650ea996294641159fd858
OxygenMemo <g58160698@gmail.com>
2017-11-27 03:06:35 +0700

    fig

--- KINDEE/application/controllers/Resturant_controller.php
+++ KINDEE/application/controllers/Resturant_controller.php
@@ -122,12 +122,8 @@ class Resturant_controller extends CI_Controller{
         $random=rand(0*10,$sum*10)/10;
         foreach($arr as $row){
             if($random>=$row->startlength && $random<=$row->endlength){
-                echo "college <br>";
-                echo $row->Res_id."<br>".$row->startlength."<br>".$row->endlength."<br>Rate :".$row->Rate_number."<hr>";
-                echo "Random : ".$random."<br>";
-            }else{
-            echo $row->Res_id."<br>".$row->startlength."<br>".$row->endlength."<br>Rate :".$row->Rate_number."<hr>";
-            echo "Random : ".$random."<br>";
+                echo json_encode($row);
+                break;
             }
            
             
